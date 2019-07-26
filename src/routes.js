84d30be06e44ec1209e4fd12f926abe46c03227b
Hover Cortez leite <hclcortez@gmail.com>
2019-07-25 23:26:08 -0300

    CRUD Ads

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
+const Adcontroller = require('./app/controllers/Adcontroller')
 
 const authMiddleware = require('./app/middlewares/auth')
 
@@ -11,7 +12,15 @@ routes.post('/users', UserController.store)
 
 routes.post('/sessions', SessionController.store)
 
-routes.get('/teste', authMiddleware, (req, res) => res.json({ok:true}))
+routes.use(authMiddleware)
+
+routes.get('/ads', Adcontroller.index)
+routes.get('/ads/:id', Adcontroller.show)
+routes.post('/ads', Adcontroller.store)
+routes.put('/ads/:id', Adcontroller.update)
+routes.delete('/ads/:id', Adcontroller.destroy)
+
+
 
 
 module.exports = routes
